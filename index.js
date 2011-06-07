ec549a31edda791c701d19ec6a3ba5d66b036dab
Evan Owen <kainosnoema@gmail.com>
2011-06-06 22:39:35 -0400

    updated comments

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+//     AJS 0.0.1
+//     (c) 2011 Evan Owen, LifeKraze LLC.
+//     AJS may be freely distributed under the MIT license.
+//     For details and documentation:
+//     http://github.com/kainosnoema/ajs
+
+// AJS
+// -------------
+
+// An experimental asyncronous templating language similar to EJS.
+
 module.exports = require('./lib/ajs');
+
+// [ajs.js](ajs.html)
+// -------------
+
+// [vm.js](vm.html)
+// -------------
+
+// [loader.js](loader.html)
+// -------------
+
+// [compiler.js](compiler.html)
+// -------------
+
+// [parser.js](parser.html)
+// -------------
+
+// [lexer.js](lexer.html)
+// -------------
+
+// [grammar.js](grammar.html)
+// -------------
