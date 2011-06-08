bb57aae3ded38000bdac6b4ce5cd31b5244c6af8
Evan Owen <kainosnoema@gmail.com>
2011-06-07 20:33:20 -0400

    added long template test to benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,9 +1,9 @@
 var ejs = require('ejs')
   , ajs = require('ajs')
-  , str = '<% if (foo) { %></p><%= foo %></p><% } %>'
-    times = 50000;
+  , str = '<% if (foo) { %><p><%= foo %></p><% } %>'
+    times = 10000;
 
-// AJS benchmark
+// AJS benchmark (many short)
 var ajs_times = times;
 console.log('rendering AJS ' + ajs_times + ' times');
 var start = new Date;
@@ -16,7 +16,7 @@ var start = new Date;
   });
 })(1);
 
-// EJS benchmark
+// EJS benchmark (many short)
 function ejsTest() {
   var ejs_times = times;
   console.log('rendering EJS ' + ejs_times + ' times');
@@ -25,4 +25,32 @@ function ejsTest() {
     ejs.render(str, { cache: true, filename: 'test', locals: { foo: 'bar' }});
   }
   console.log('EJS took ' + (new Date - start) + 'ms');
+  longTest();
+}
+
+function longTest() {
+  console.log();
+  
+  var template = [];
+  while(times--) {
+    template.push(str);
+  }
+  
+  template = template.join('\n');
+  
+  // AJS benchmark (one very long)
+  console.log('rendering very long AJS');
+  var start = new Date;
+  ajs.render(template, {filename: 'test2', locals: {foo: 'bar'}}, function(result) {
+    console.log('AJS took ' + (new Date - start) + 'ms');
+    ejsLongTest();
+  });
+
+  // EJS benchmark (one very long)
+  function ejsLongTest() {
+    console.log('rendering very long EJS');
+    var start = new Date;
+    var result = ejs.render(template, { cache: true, filename: 'test2', locals: { foo: 'bar' }});
+    console.log('EJS took ' + (new Date - start) + 'ms');
+  }
 }
