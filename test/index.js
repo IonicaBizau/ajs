c7c2de67974306ccdc24d377fcce2a92b9d2da0e
Ionică Bizău <bizauionica@gmail.com>
2016-08-02 18:37:30 +0300

    Pass the error in the output

--- test/index.js
+++ test/index.js
@@ -14,12 +14,47 @@ tester.describe("ajs", t => {
         if (name.startsWith("_")) { return; }
         t.should(`handle ${name} cases`, cb => {
             ajs.render(readFile(files.inputAjs.path), {
-                filename: Math.random() + ".js"
-              , locals: require(files.inputJs.path)
-            }, (data) => {
+                locals: require(files.inputJs.path)
+            }, (err, data) => {
+                t.expect(err).toBe(null);
                 t.expect(data).toBe(readFile(files.outputHtml.path));
                 cb();
             });
         });
+    });
+
+    let templ = null;
+    t.it("compile the file", cb => {
+        ajs.compileFile(tree.simple.inputAjs.path, (err, template) => {
+            templ = template;
+            cb();
+        });
+    });
+
+    t.it("reuse the compiled result", cb => {
+        templ(require(tree.simple.inputJs.path), (err, data) => {
+            t.expect(data).toBe(readFile(tree.simple.outputHtml.path));
+            cb();
+        });
+    });
+
+    t.it("reuse the compiled result again", cb => {
+        templ(require(tree.simple.inputJs.path), (err, data) => {
+            t.expect(data).toBe(readFile(tree.simple.outputHtml.path));
+            cb();
+        });
+    });
+
+    t.it("reuse the compiled result again", cb => {
+        templ(require(tree.simple.inputJs.path), (err, data) => {
+            t.expect(data).toBe(readFile(tree.simple.outputHtml.path));
+            cb();
+        });
+    });
+
+    t.it("handle streams", cb => {
+        templ(require(tree.encode.inputJs.path)).on("data", chunk => {
+            t.expect(chunk).toBeA("string");
+        }).on("end", cb);
     });
 });
